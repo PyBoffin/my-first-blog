f8177bdb26f607a3ad8a89e1a09e93b092e784ae
Vaishakh <pyboffin@gmail.com>
2018-09-25 16:50:33 +0530

    Changed the HTMl for the Site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#Views are just Python functions that are a little bit more
+#complicated than the ones we wrote in the Introduction to Python chapter.
+
+def post_list(request):
+    return render(request,'blog/post_list.html',{})
+    
